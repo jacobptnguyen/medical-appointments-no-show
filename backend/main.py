6f59be7517b85666bb7cabd4fc5c84b3af8b4c5b
Jacob Nguyen <jacobptnguyen@gmail.com>
2025-11-11 23:53:44 -0800

    fixing deployment issues

--- backend/main.py
+++ backend/main.py
@@ -135,3 +135,9 @@ def get_neighbourhoods():
     ]
     return {"neighbourhoods": neighbourhoods}
 
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
